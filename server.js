a52b9328b2ccd708ff2db7cf099864cced07b382
Sakpal Amit <amitsakpal744@gmail.com>
2024-08-03 20:50:44 +0530

    env setup and etc

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 
 const port = 3000 || process.env.PORT;
 
+const dotenv = require('dotenv')
 
 // basic get boilerplae  // 
+
+dotenv.config()
 
 app.get('/',(req,res) => {
     res.send('welcome to BLACKND')
