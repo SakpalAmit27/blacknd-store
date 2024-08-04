87745e7b6ea86f6eee00aabd8c1999666bd84074
Sakpal Amit <amitsakpal744@gmail.com>
2024-08-04 11:47:23 +0530

    implementing the controller for signup and signin

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+// controller to handle user request eg : login and signup .// 
+
+// ex / 
+
+import express from 'express'
+
+// adding our user model here // s
+
+import User from '../models/userModel'
+
+// router for routing // 
+const router = express.Router();
+
+
