15b0dd5fab15fc64c1e2cf8cf252420d03e6a5b0
Sakpal Amit <amitsakpal744@gmail.com>
2024-08-04 11:57:24 +0530

    jwt imports and pkg , progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,4 +11,40 @@ import User from '../models/userModel'
 // router for routing // 
 const router = express.Router();
 
+// bcrypt for hashing // 
 
+import bcrypt from 'bcrypt'
+
+
+import crypto from 'crypto'
+// json web token .. 
+
+import jwt from 'jsonwebtoken'
+
+// 1 : async function to register the user .. 
+
+const RegisterUser = async (req,res) => {
+    // essentials // 
+
+    const {name,email,password} = req.body;
+
+    try{
+        // finding the user email if he exists already// 
+
+
+        let user = User.findOne({email});
+
+        if(user){
+            return res.status(400).json({message:"user already exists , please login"})
+        }
+
+        // if he doesnt exist // 
+
+        // hashing the password .. 
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+
+
+    }
+}
