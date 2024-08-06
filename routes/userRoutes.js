91ffab9aa84a90d25fa88aa10277d764b04ee3ac
Sakpal Amit <amitsakpal744@gmail.com>
2024-08-06 20:36:31 +0530

    improved validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,28 @@ import express from 'express'
 import RegisterUser from '../controllers/userController.js';
 // login controller // 
 import LoginUser from '../controllers/user_loginController.js';
+
+// import check // 
+
+import { check } from 'express-validator';
+
 const router = express.Router();
 
 // creating the endpoint .. 
 
-router.post('/register',RegisterUser);
-router.post('/login',LoginUser)
+router.post('/register',[
+    check('name','name is required').not().isEmpty(),
+    check('email','please include an valid email').isEmail(),
+    check('password','password must be minimum 6').isLength({min : 6})
+],
+RegisterUser
+);
+router.post('/login',[
+    check('email','please include an valid email').isEmail(),
+    check('password','password must be minimum 6').isLength({min : 6})
+],
+LoginUser
+)
 
 export default router;
 // export the router // 
